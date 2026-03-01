b21389225e00d5dec61960c499b3857fdda321c2
liujinbf <88815755+liujinbf@users.noreply.github.com>
2026-03-01 16:53:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,8 +3,8 @@
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall-luci' >>feeds.conf.default
 
-# 2. 拉取 fanchmwrt 的专属 Design 主题
-git clone https://github.com/fanchmwrt/luci-theme-design.git package/luci-theme-design
+# 2. 添加 Design 主题源 (改为 feed 方式)
+echo 'src-git design https://github.com/fanchmwrt/luci-theme-design' >>feeds.conf.default
 
-# 3. 添加额外依赖包源
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# 3. 添加额外依赖包源 (改为 feed 方式)
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
